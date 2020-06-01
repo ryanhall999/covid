a685a86d549f3076e228a53378679d8a9dbc8f00
Ryan Hall <ryanhall999@gmail.com>
2020-06-01 11:51:51 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -20,11 +20,16 @@ function App() {
 	const handleClick = async (e) => {
 		e.preventDefault();
 		await axios.get("https://api.covid19api.com/summary").then((response) => {
-			setGlobal(response.data.Global);
-			setUs(response.data.Countries[177]);
+			console.log(response);
+			if (response.status === "429") {
+				alert("Too Many Requests, Please Try Again");
+			} else {
+				setGlobal(response.data.Global);
+				setUs(response.data.Countries[177]);
+			}
 		});
 		// await axios
-		// 	.get("https://covidtracking.com/api/states/daily")
+		// 	.get("https://covidtracking.com/api/states/daily")ß
 		// 	.then((response) => {
 		// 		console.log(response);
 		// 		setDays(response.data);
@@ -64,47 +69,56 @@ function App() {
 
 	return (
 		<div>
-			<div>
-				<Router>
-					<Navbar bg="dark" variant="dark">
-						<Navbar.Brand href="#home">Ryan's Covid Tracker</Navbar.Brand>
-						<Nav className="mr-auto">
-							<Nav.Link href="#home">Home</Nav.Link>
-							<Nav.Link href="#lists">Lists</Nav.Link>
-							<Nav.Link href="#graphs">Graphs</Nav.Link>
-						</Nav>
-						<Route path="#lists" component={MakeStates} exact />
-						<Button variant="primary" onClick={handleClick}>
-							Refresh
-						</Button>
-					</Navbar>
+			<Router>
+				<div>
+					<nav>
+						<div id="navbar">
+							<span>Ryan's Covid Tracker</span>
+							<Link to="/">Home</Link>
+							<Link to="/list">List</Link>
+							<Link to="/graphs">Graphs</Link>
+							<Button variant="primary" onClick={handleClick}>
+								Refresh
+							</Button>
+						</div>
+					</nav>
 					<Switch>
-						<Route path="#home">
+						<Route path="/list">
+							<MakeStates states={states} />
+						</Route>
+						<Route path="/graphs">
+							<Graphs states={states} />
+						</Route>
+						<Route path="/">
 							<div id="main">
 								<Global global={global} />
 								<US us={us} />
 								<USAMap onClick={mapHandler} />
 							</div>
 						</Route>
-						<Route path="#lists">
-							<MakeStates states={states} />
-						</Route>
-						<Route path="#graphs"></Route>
 					</Switch>
-				</Router>
-			</div>
-			<div>
-				<div id="main">
-					<Global global={global} />
-					<US us={us} />
-					<USAMap onClick={mapHandler} />
-					<Graphs states={states} />
-					<MakeStates states={states} />
 				</div>
-			</div>
+			</Router>
+			;
 		</div>
 	);
 }
 
 export default App;
-//
+
+// <div>
+// 	<Router>
+// 		<Navbar bg="dark" variant="dark">
+// 			<Navbar.Brand href="#home">Ryan's Covid Tracker</Navbar.Brand>
+// 			<Nav className="mr-auto">
+// 				<Nav.Link href="#home">Home</Nav.Link>
+// 				<Nav.Link href="#lists">Lists</Nav.Link>
+// 				<Nav.Link href="#graphs">Graphs</Nav.Link>
+// 			</Nav>
+// 			<Route path="#lists" component={MakeStates} exact />
+// 			<Button variant="primary" onClick={handleClick}>
+// 				Refresh
+// 			</Button>
+// 		</Navbar>
+// 	</Router>
+// </div>;
